33a6589592b556f5d2ac7ed35ccf88f55958a087
Rakelg <63641968+Rakelg@users.noreply.github.com>
2021-09-02 14:58:49 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -1,16 +1,19 @@
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-# algorithm : next number in the sequence is the sum of last 3 numbers, the program needs to go 3 numbers back and find the sum. if 1,2 or 3 the program cant go 3 numbers back so that is hardcoded. 
+# algorithm : curr_num in the sequence is the sum of last 3 numbers
+# the program needs to go 3 numbers back and find the sum. 
+# if 1,2 or 3 is entered the program program prints the first 1-3 numbers of the sequence. 
 
 
-for i in range(1, n+1):
-    if i == 1:
-        current = first = i
-    elif i == 2:
-        current = second = i
-    elif i == 3:
-        current = third = i
-    else:
-        current = first + second + third
-        first, second, third = second, third, current
-    print(current)
+for i in range(1, n+1): 
+    if i == 1: 
+        curr_num = first = i
+    elif i == 2: 
+        curr_num = second = i 
+    elif i == 3: 
+        curr_num = third = i 
+    else : 
+        curr_num = first + second + third
+        first, second, third = second,third,curr_num
+    print(curr_num)
+    
